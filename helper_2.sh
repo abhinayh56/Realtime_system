7c0d41efc52942fb9d374370a2d7c23767502b9f
abhinayh56 <kumarabhinay53@gmail.com>
2025-05-19 13:58:32 +0530

    asdfasdf

--- helper_2.sh
+++ helper_2.sh
@@ -3,14 +3,10 @@
 echo -e "PID\tCPU\tCOMMAND\t\t\tTYPE"
 echo "------------------------------------------------------------"
 
-# Get processes on CPU 4 or 5
 ps -eo pid,psr,comm --no-headers | grep -E "\s(4|5)$|\s(4|5)\s" | while read pid cpu comm; do
-    # Check if the /proc/[pid]/exe symlink exists
-    if [ -L /proc/$pid/exe ]; then
-        # User-space process
+    if [ -e "/proc/$pid/exe" ] && readlink "/proc/$pid/exe" &> /dev/null; then
         type="User"
     else
-        # Kernel thread
         type="Kernel"
     fi
 
